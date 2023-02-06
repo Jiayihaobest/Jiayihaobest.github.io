071c01144a9b1b7dc7f1fe9b374ee9d49763d67f
Jiayi Hao <hao.jiay@northeastern.edu>
2023-02-06 00:35:37 -0500

    0131final

--- homework/01-31/scripts/myFunctions.js
+++ homework/01-31/scripts/myFunctions.js
@@ -1,15 +1,50 @@
-// A series of custom functions. This file can be considered
-// as a custom library, and each of the functions below can be
-// called from an html page after this js file has been loaded.
+let data = [];
 
-function sumAB(A, B) {
-
-    return A + B;
-
+function updateData() {
+	data = [];
+	let numItems = Math.ceil(Math.random() * 10);
+	for(let i=0; i<numItems; i++) {
+		data.push(Math.random() * 800);
+	}
 }
 
-function randomPosition() {
-
-    return Math.random() * 140 + 70;
-    
+function update() {
+	d3.select('svg')
+		.selectAll('circle')
+		.data(data)
+		.join(
+			function(enter) {
+				return enter
+					.append('circle')
+					.attr('cy', 50)
+					.attr('cx', function(d) {
+						return d;
+					})
+					.attr('r', 40)
+					.style('opacity', 0);
+			},
+			function(update) {
+				return update;
+			},
+			function(exit) {
+				return exit
+					.transition()
+					.duration(1000)
+					.attr('cy', 500)
+					.remove();
+			}
+		)
+		.transition()
+		.duration(1000)
+		.attr('cx', function(d) {
+			return d;
+		})
+		.style('opacity', 0.75);
 }
+
+function updateAll() {
+	updateData();
+	update();
+}
+
+updateAll();
